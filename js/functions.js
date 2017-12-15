872409b0ce522030dccaee11d589e2788f760930
AnthonyChamberlin <antchamberlin.js@gmail.com>
2017-12-15 14:55:33 +0000

    Write jQuery for Floating and Landing Recent Pizza Order Divs

--- js/functions.js
+++ js/functions.js
@@ -30,6 +30,11 @@ $(window).scroll(function(){
     'transform' : 'translate(0px, '+ windowScroll /2 +'%)'
   });
 
+  // Title and Text fade-ins
+  if(windowScroll > $('.subtitle').offset().top - $(window).height()) {
+    $('.subtitle').animate({opacity: 1, marginTop: -50}, 1500);
+  }
+
   // Items Display Fade In
   if(windowScroll > $('.item-display').offset().top - $(window).height() / 1.2) {
     $('.item-display figure').each(function(i){
@@ -42,16 +47,28 @@ $(window).scroll(function(){
 
   // Telescope Promo's
   if(windowScroll > $('.telescope-display-1').offset().top - $(window).height() ) {
-    $('.telescope-display-1').css({'background-position':'center ' + windowScroll + 'px'});
+    $('.telescope-display-1').css({'background-position':'center ' + (windowScroll - $('.telescope-display-1').offset().top) + 'px'});
+
+    var opacity = (windowScroll - $('.telescope-display-1').offset().top + 400) / (windowScroll / 4)
+    $('.telescope-tint').css({ 'opacity': opacity })
   }
 
-  // Telescope Promo's
-  if(windowScroll > $('.telescope-display-2').offset().top - $(window).height() ) {
-    $('.telescope-display-2').css({'background-position':'center ' + windowScroll + 'px'});
+
+
+  // Recent Orders Floating Scroll
+  if(windowScroll > $('.recent-orders-1').offset().top - $(window).height()) {
+
+    var offsetOneThree = Math.min(0, windowScroll - $('.recent-orders-1').offset().top + $(window).height() - 400);
+
+    var offsetTwo = Math.min(0, windowScroll - $('.recent-orders-1').offset().top + $(window).height() - 400);
+
+    var orderOpacity1 = (windowScroll - $('.recent-orders-1').offset().top + 400) / (windowScroll / 4)
+    var orderOpacity2 = (windowScroll - $('.recent-orders-1').offset().top + 450) / (windowScroll / 6)
+
+    $('.order-1').css({'transform':'translate('+ offsetOneThree +'px, 20px)', 'opacity': orderOpacity2});
+    $('.order-2').css({'transform':'translate(0px, '+ Math.abs(offsetTwo) +'px)', 'opacity': orderOpacity2});
+    $('.order-3').css({'transform':'translate('+ Math.abs(offsetOneThree) +'px, 20px)', 'opacity': orderOpacity2  });
+
   }
 
-  // Telescope Promo's
-  if(windowScroll > $('.telescope-display-3').offset().top - $(window).height() ) {
-    $('.telescope-display-3').css({'background-position':'center ' + windowScroll + 'px'});
-  }
 });
